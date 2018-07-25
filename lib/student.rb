5e07e9592819f97676aef86bf0c63b529bb638ab
scarlettperry <si.perry15@gmail.com>
2018-07-25 13:57:56 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,51 @@
-class Student
+require 'pry'
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+class Student
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  # def initialize(attributes)
+  #   @id = attributes[:id] #:id(symbol) vs id:(key to hash)
+  #   @name = attributes[:name]
+  #   @grade = attributes[:grade]
+  # end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT * FROM students")[0][0]
+    #binding.pry
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 end
